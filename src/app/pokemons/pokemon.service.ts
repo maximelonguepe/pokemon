9b2a1e89141496f4c325e1e48d629c8c9e6bc1f7
maxlamare <58212074+maxlamare@users.noreply.github.com>
2022-03-11 13:44:46 +0100

    tp1 pokedex a jour

--- src/app/pokemons/pokemon.service.ts
+++ src/app/pokemons/pokemon.service.ts
@@ -9,16 +9,16 @@ import {Pokemondetail} from "./models/pokemondetail";
   providedIn: 'root'
 })
 export class PokemonService {
-  private heroesUrl='http://app-ec21e68e-3e55-42d7-b1ae-3eef7507a353.cleverapps.io/pokemons';
+  private pokemonUrl='http://app-ec21e68e-3e55-42d7-b1ae-3eef7507a353.cleverapps.io/pokemons';
   constructor(private http:HttpClient) { }
 
   getPokemons(): Observable<Pagedata<Pokemon>> {
     const params =new HttpParams().set('offset',0).set('limit',1000);
-    return this.http.get<Pagedata<Pokemon>>(this.heroesUrl,{params});
+    return this.http.get<Pagedata<Pokemon>>(this.pokemonUrl,{params});
   }
 
   getPokemon(id: number | undefined):Observable<Pokemondetail>{
-    return this.http.get<Pokemondetail>(this.heroesUrl+'/'+id);
+    return this.http.get<Pokemondetail>(this.pokemonUrl+'/'+id);
   }
 
 }
