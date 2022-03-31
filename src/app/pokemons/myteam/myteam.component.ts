0a5cfeb2ae231b5bc598d97eafc41b9dc4ce1346
maximelonguepe <longuepemaxime73@gmail.com>
2022-03-31 23:23:40 +0200

    connexion done

--- src/app/pokemons/myteam/myteam.component.ts
+++ src/app/pokemons/myteam/myteam.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {CookieService} from "ngx-cookie-service";
+import {MyteamService} from "./myteam.service";
 
 @Component({
   selector: 'app-myteam',
@@ -8,16 +9,20 @@ import {CookieService} from "ngx-cookie-service";
   styleUrls: ['./myteam.component.scss']
 })
 export class MyteamComponent implements OnInit {
+  private token?:string;
+  private array?:number[];
+  constructor(private router:Router, private cookie : CookieService, private myteamService : MyteamService) { }
 
-  constructor(private router:Router, private cookie : CookieService) { }
+   ngOnInit(): void{
 
-  ngOnInit(): void {
-    let token = this.cookie.get('token');
-    if(token!=""){
-      console.log(token);
-
-    }
-    else{
+    this.token = this.cookie.get('token');
+    if (this.token != "") {
+      console.log(this.token);
+      this.myteamService.getTeam(this.token).subscribe(pagedata => this.array = pagedata);
+      console.log("-->" + this.array);
+      this.myteamService.getTeam(this.token).subscribe(pagedata => this.array = pagedata);
+      console.log("-->" + this.array);
+    } else {
       console.log("vide")
       this.router.navigate(['connexion']);
 
--- src/app/pokemons/myteam/myteam.service.ts
+++ src/app/pokemons/myteam/myteam.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Pagedata} from "../models/pagedata";
+import {Pokemon} from "../models/pokemon.model";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyteamService {
+  private myTeamUrl = 'http://app-ec21e68e-3e55-42d7-b1ae-3eef7507a353.cleverapps.io/trainers/me/team';
+  constructor(private http: HttpClient) {
+  }
 
-  constructor() { }
+  getTeam(token:string): Observable<number[]> {
+    return this.http.get<number[]>(this.myTeamUrl , {
+      headers: {
+        "Authorization": 'Bearer ' + token
+      }});
+  }
+
 }
